7520da3a9583d0faa7001234ce65c8aac2e569ac
David McAfee <mcafd@amazon.com>
2023-08-03 22:42:27 -0700

    composite key repro

---
+++ 8-2-opt-managed-composite-keys-639/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "82optmanagedcomposit": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
