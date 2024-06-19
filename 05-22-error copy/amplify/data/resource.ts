8a277977cf4cdd107ce0e287e81f2dea7ea33036
David McAfee <mcafd@amazon.com>
2024-06-19 15:54:06 -0700

    update

---
+++ 05-22-error copy/amplify/data/resource.ts
@@ -0,0 +1,30 @@
+import { a, defineData, type ClientSchema } from "@aws-amplify/backend";
+
+const schema = a.schema({
+  Todo: a
+    .model({
+      title: a.string(),
+      requiredField: a.string().required(),
+      content: a.hasOne("Content", "todoId"),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
+  Content: a
+    .model({
+      todoId: a.id(),
+      todo: a.belongsTo("Todo", "todoId"),
+      text: a.string(),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
+});
+
+// Used for code completion / highlighting when making requests from frontend
+export type Schema = ClientSchema<typeof schema>;
+
+// defines the data resource to be deployed
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "apiKey",
+    apiKeyAuthorizationMode: { expiresInDays: 30 },
+  },
+});
---
+++ 05-22-error copy/src/App.tsx
@@ -0,0 +1,121 @@
+import { useEffect, useState } from "react";
+import type { Schema } from "../amplify/data/resource";
+import type { SelectionSet } from "aws-amplify/data";
+import { generateClient } from "aws-amplify/data";
+
+const client = generateClient<Schema>();
+
+// const selectionSet = ["id", "title", "content.*"] as const;
+// const selectionSet2 = ["id", "title"] as const;
+
+// type TodoModel = SelectionSet<Schema["Todo"]["type"], typeof selectionSet>;
+
+function App() {
+  // const [todos, setTodos] = useState<TodoModel[]>([]);
+
+  async function listTodos() {
+    // fetch all todos
+    // const { data } = await client.models.Todo.list({
+    //   selectionSet: selectionSet,
+    // });
+    const { data } = await client.models.Todo.list();
+    // setTodos(data);
+
+    console.log("listTodos", data);
+  }
+
+  // useEffect(() => {
+  //   listTodos();
+  // }, []);
+
+  async function retrieveChildRecords(items: any) {
+    const test = await Promise.all(
+      items.map(async (i: any) => {
+        const result = await i.content();
+        console.log("child result", result);
+        return result;
+      })
+    );
+
+    console.log("child records", test);
+  }
+
+  async function getChildrenFromList() {
+    const { data, errors: listErrors } = await client.models.Todo.list();
+    console.log("list:", data);
+    console.log("listErrors", listErrors);
+    await retrieveChildRecords(data);
+  }
+
+  async function deleteAll() {
+    const { data } = await client.models.Todo.list();
+
+    for (const d of data) {
+      await client.models.Todo.delete(d);
+    }
+
+    const { data: contentData } = await client.models.Content.list();
+
+    for (const c of contentData) {
+      await client.models.Content.delete(c);
+    }
+  }
+
+  useEffect(() => {
+    const sub = client.models.Todo.observeQuery({
+      // Cx selectionSet results in following error:
+      // `The type is readonly and cannot be assigned to the mutable type:`
+      // selectionSet: selectionSet,
+      // selectionSet: ["id", "title", "requiredField", "content.id"],
+      selectionSet: ["id", "title"],
+    }).subscribe(({ items, isSynced }) => {
+      // const sub = client.models.Todo.observeQuery().subscribe(
+      //   ({ items, isSynced }) => {
+      console.log("observeQuery:", items);
+      console.log("isSynced", isSynced);
+
+      // for (const i of items) {
+      //   if (typeof i.content === "function") {
+      //     // debugger;
+      //     // // @ts-ignore
+      //     // const test = await i.content();
+      //     // debugger;
+      //     // test2(i);
+      //   }
+      // }
+      // retrieveChildRecords(items);
+    });
+
+    return () => sub.unsubscribe();
+  }, []);
+
+  return (
+    <div>
+      <h1>Todos</h1>
+      <button
+        onClick={async () => {
+          const { data: newTodo } = await client.models.Todo.create({
+            requiredField: "test",
+          });
+
+          await client.models.Content.create({
+            text: "Content",
+            todoId: newTodo?.id,
+          });
+
+          await listTodos();
+        }}
+      >
+        Create
+      </button>
+
+      <button onClick={listTodos}>list</button>
+      <button onClick={deleteAll}>delete all</button>
+      <button onClick={getChildrenFromList}>
+        list parent then retrieve children
+      </button>
+    </div>
+  );
+}
+
+export default App;
